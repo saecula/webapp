e985ee82cbd49a9f4dcca9431c344acc5a76dd92
Kathleen Gilbert <krgilb@gmail.com>
2022-04-02 20:50:01 -0400

    dumb bg

--- gogo/src/Webapp.js
+++ gogo/src/Webapp.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./webapp.css";
 
-const BOARDSTATES = {
+const BOARD_STATES = {
   empty: "empty",
   black: "black",
   white: "white",
 };
+const STONE_COLORS = {
+  black: "black",
+  white: "white",
+};
+
 const board = Array(19)
   .fill(null)
   .map((_, rowNum) =>
@@ -15,7 +20,7 @@ const board = Array(19)
       .map((r, c) => ({
         row: r,
         col: c,
-        state: BOARDSTATES.empty,
+        state: BOARD_STATES.empty,
       }))
   );
 
@@ -34,16 +39,62 @@ const Webapp = () => {
     }
   };
 
+  const [stoneColor] = useState(STONE_COLORS.black);
   const [gameState, setGameState] = useState(board);
+
+  const setStone = (key) => {
+    const [cRow, cCol] = key.split(":").map((s) => parseInt(s));
+    console.log("current row and column:", cRow, cCol);
+    setGameState((prevGameState) => {
+      const prevState = prevGameState && prevGameState[cRow][cCol].state;
+      console.log("prev state:", prevState);
+      const newthing = prevGameState.map((prevRow) =>
+        prevRow.map((sq) => {
+          console.log(
+            "trying to compare",
+            sq.row,
+            "to",
+            cRow,
+            "and",
+            sq.col,
+            "to",
+            cCol
+          );
+          if (sq.row === cRow && sq.col === cCol) {
+            console.log("FOUND");
+            return {
+              ...sq,
+              state:
+                prevState === BOARD_STATES.empty
+                  ? stoneColor
+                  : BOARD_STATES.empty,
+            };
+          } else {
+            return { ...sq };
+          }
+        })
+      );
+      console.log("hm", newthing);
+      return newthing;
+    });
+  };
 
   const makePlayingSquare = (rowNum, colNum) => {
     const key = rowNum + ":" + colNum;
-    return rowNum != 0 && colNum != 0 ? (
-      <div key={key} className="playing-square">
-        <div className="visible-square" />
+    return (
+      <div
+        key={key}
+        id={key}
+        className={`playing-square ${
+          gameState && gameState[rowNum] && gameState[rowNum][colNum]?.state
+        }`}
+        onClick={() => {
+          console.log("hello", key);
+          setStone(key);
+        }}
+      >
+        {/* {rowNum != 0 && colNum != 0 && <div className="visible-square" />} */}
       </div>
-    ) : (
-      <div key={key} className="playing-square" />
     );
   };
 
@@ -52,8 +103,8 @@ const Webapp = () => {
       <header className="webapp-header">
         <div id="board-container">
           <div id="playing-area">
-            {board.map((rows, rowNum) =>
-              rows.map((_, colNum) => makePlayingSquare(rowNum, colNum))
+            {board.map((rows, y) =>
+              rows.map((_, x) => makePlayingSquare(y, x))
             )}
           </div>
         </div>
